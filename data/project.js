9785d772cee7db6b34ab30b00c95b2b9e73b9e3c
ProbestGenesis <dimonange4@gmail.com>
2024-12-29 20:49:32 +0000

    Update project.js

--- data/project.js
+++ data/project.js
@@ -3,8 +3,8 @@ export const project = [{
     'Name' : "BOUTIQUE EN LIGNE",
     'Description' : "Ce fut l'un des tout premiers projets que j'ai entrepris après avoir acquis les bases des frameworks Express, React et Redux. Bien que l'application soit complètement fonctionnelle, mes lacunes en UI/UX à l'époque la rendaient peu attrayante.",
     'Status' : "END",
-    'StartDate' : "2023-09-17",  
-    'EndDate' : "2023-12-03",
+    'StartDate' : "2024-02-17",  
+    'EndDate' : "2024-04-20",
     'Owner' : "DIMON OYETOUNDE ANGE",
     "image" : ""
 }, {
